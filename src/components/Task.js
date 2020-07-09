2e67992a31c3c79b8ca04040823f67ac8c8f59f1
verobega <pooh_tres@hotmail.com>
2020-07-09 00:56:23 -0500

    doing myToDo components

---
+++ src/components/Task.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Task = (props) => {
+
+    const{
+        title, done, category
+    } = props;
+    return (
+        <div>
+            <h1>Task</h1>
+        </div>
+    )
+}
+
+export default Task
---
+++ src/containers/MainContainer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../components/Navbar';
+import Task from '../components/Task';
+
+const MainContainer = () => {
+    return (
+        <div>
+            <Navbar />
+            
+            <Task />
+        </div>
+    )
+}
+
+export default MainContainer
